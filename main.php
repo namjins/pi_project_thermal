05119baaff52b467c221b7c0f2141e6308580b33
Marc Snijman <namjins@me.com>
2018-04-25 03:12:39 +0000

    main.php has the bulk of the code complete.  Currently working on implementing the audio to play.

--- main.php
+++ main.php
@@ -1,3 +1,24 @@
 <?php
+// Original idea and code: https://github.com/colinodell/php-iot-examples
+const GPIO_PIN = 17;
 
+require_once 'vendor/autoload.php';
+
+use PiPHP\GPIO\GPIO;
+use PiPHP\GPIO\Pin\InputPinInterface;
+
+$audio_scream = `omxplayer -o local audio/example.mp3`;
+
+$gpio = new GPIO();
+$pin = $gpio->getInputPin(GPIO_PIN);
+$pin->setEdge(InputPinInterface::EDGE_RISING);
+
+$interruptWatcher = $gpio->createWatcher();
+$interruptWatcher->register($pin, function(InputPinInterface $pin, $value) {
+	echo $audio_scream;
+});
+
+while(true) {
+
+}
 ?>
