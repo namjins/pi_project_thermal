d5bb41038ed8c86a6493f123bd7805a0e042f38a
Marc Snijman <namjins@me.com>
2018-04-30 19:25:02 -0600

    Class created for porch. Cleaned up main.php

--- main.php
+++ main.php
@@ -1,24 +1,31 @@
 <?php
 // Original idea and code: https://github.com/colinodell/php-iot-examples
 const GPIO_PIN = 17;
+const TIMEOUT = 60 * 5; // 5 minutes
+const ACTIVATION_THRESHOLD = 2;
 
 require_once 'vendor/autoload.php';
 
 use PiPHP\GPIO\GPIO;
 use PiPHP\GPIO\Pin\InputPinInterface;
 
-$audio_scream = `omxplayer -o local audio/example.mp3`;
+$active_start = new DateTime('2000-01-01 21:00:00');
+$active_stop = new DateTime('2000-01-01 06:00:00');
 
+$porch = new Porch($active_start, $active_stop, TIMEOUT, ACTIVATION_THRESHOLD);
 $gpio = new GPIO();
 $pin = $gpio->getInputPin(GPIO_PIN);
 $pin->setEdge(InputPinInterface::EDGE_RISING);
 
 $interruptWatcher = $gpio->createWatcher();
-$interruptWatcher->register($pin, function(InputPinInterface $pin, $value) {
-	echo $audio_scream;
+$interruptWatcher->register($pin, function (InputPinInterface $pin, $value) use ($porch) {
+    $porch->motionDetected();
 });
 
-while(true) {
+while (true) {
+    // Keeps watcher active
+    $interruptWatcher->watch(TIMEOUT * 1000);
+    $porch->resetActivationCount();
 
 }
 ?>
---
+++ src/Porch.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marc-PC
+ * Date: 4/30/2018
+ * Time: 6:02 PM
+ */
+
+class Porch
+{
+    /**
+     * @var int
+     */
+    private $last_movement;
+
+    /**
+     * @var int
+     */
+    private $active_start;
+
+    /**
+     * @var int
+     */
+    private $active_stop;
+
+    /**
+     * @var int
+     */
+    private $activation_count;
+
+    /**
+     * @var int
+     */
+    private $activation_threshold;
+
+    /**
+     * @var int
+     */
+    private $timeout;
+
+    /**
+     * @var string
+     */
+    private $audio_scream;
+
+
+    /**
+     * Porch constructor.
+     */
+    public function __construct(DateTime $active_start, Datetime $active_stop, $timeout, $activation_threshold)
+    {
+        $this->last_movement = time();
+        $this->active_start = date("G", $active_start);
+        $this->active_stop = date("G", $active_stop);
+        $this->activation_threshold = $activation_threshold;
+        $this->activation_count = 0;
+        $this->timeout = $timeout;
+        $this->audio_scream = `omxplayer -o local audio/nmh_scream1.mp3`;
+    }
+
+    /**
+     * Call this function whenever there is motion.
+     *
+     * @return void
+     */
+    public function motionDetected()
+    {
+        $this->increaseActivationCount();
+        $new_movement = time();
+
+        $last_movement = $this->getHour($this->last_movement);
+        // Check that we're within our time window
+        if ($last_movement > $this->active_start && $last_movement < $this->active_stop) {
+            if ((($new_movement - $this->last_movement) > $this->timeout || $this->activation_count <= $this->activation_threshold)) {
+                $this->activateSound();
+            }
+        }
+        $this->last_movement = $new_movement;
+    }
+
+    /**
+     * Call this to reset $activation_threshold
+     *
+     * @return int
+     */
+    private function getHour($timestamp)
+    {
+        return date("G", new DateTime($timestamp));
+
+    }
+
+    /**
+     * Call this to reset $activation_threshold
+     *
+     * @return void
+     */
+    public function resetActivationCount()
+    {
+        if (time() - $this->last_movement >= $this->timeout) {
+            $this->activation_count = 0;
+        }
+    }
+
+    /**
+     * Call this function whenever there is motion.
+     *
+     * @return void
+     */
+    private function increaseActivationCount()
+    {
+        $this->activation_count++;
+    }
+
+    /**
+     * Call this function when playing sound
+     *
+     * @return void
+     */
+    private function activateSound()
+    {
+        echo $this->audio_scream;
+    }
+}
