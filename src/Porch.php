6dc8d32646fb140b3ce44a8aaaab892f40bb4534
Marc Snijman <namjins@me.com>
2018-04-30 19:51:07 -0600

    Additional corrections to timestamp handling

--- src/Porch.php
+++ src/Porch.php
@@ -50,8 +50,8 @@ class Porch
     public function __construct(DateTime $active_start, Datetime $active_stop, $timeout, $activation_threshold)
     {
         $this->last_movement = time();
-        $this->active_start = date("G", strtotime($active_start));
-        $this->active_stop = date("G", strtotime($active_stop));
+        $this->active_start = date("G", $active_start->getTimestamp());
+        $this->active_stop = date("G", $active_stop->getTimestamp());
         $this->activation_threshold = $activation_threshold;
         $this->activation_count = 0;
         $this->timeout = $timeout;
@@ -85,8 +85,7 @@ class Porch
      */
     private function getHour($timestamp)
     {
-        return date("G", new DateTime($timestamp));
-
+        return date("G", $timestamp);
     }
 
     /**
